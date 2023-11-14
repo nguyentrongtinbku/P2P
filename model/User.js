796d00df20254e7c8669e9041cd4e69451d96122
thieentraan0412 <you@example.com>
2023-11-14 09:14:42 +0700

    server

---
+++ model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    ipv4: {
+      type: String,
+    },
+    file: [
+      {
+        file_id: { type: String },
+        path: { type: String },
+        name_file: { type: String },
+      },
+    ],
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,123 +1,197 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const os = require("os");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3000; 
-mongoose.connect('mongodb://localhost/MMT', { useNewUrlParser: true, useUnifiedTopology: true });
+const port = 8080;
+const cors = require("cors");
+const User = require("./model/User");
 
-const fileSchema = new mongoose.Schema({
-    clientIp: String,
-    localFilePath: String,
-    serverFileName: String,
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-const File = mongoose.model('File', fileSchema);
+async function connect() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/MMT", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected in mongdb");
+  } catch (error) {
+    console.log("ko ket noi duoc");
+  }
+}
+connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-app.post('/publish', async (req, res) => {
-    const { localFilePath, serverFileName } = req.body;
-    const clientIp = req.ip;
-    try {
-      const file = new File({ clientIp, localFilePath, serverFileName });
-      await file.save();
-    } catch (err) {
-      res.status(500).json({ error: 'An error occurred while saving the file information.' });
-    }
+app.post("/register", async (req, res) => {
+  const networkInterfaces = os.networkInterfaces();
+  const clientIp = networkInterfaces["Wi-Fi"][1].address;
+  const newData = new User({
+    username: req.body.username,
+    password: req.body.password,
+    ipv4: clientIp,
+    isAdmin: false,
   });
-
-app.post('/fet', async (req, res) => {
-    try {
-      const serverFileName = req.body;
-      const fileInfo = await File.findOne({ serverFileName });
-      if (!fileInfo) {
-        res.status(404).json({ error: 'File not found' });
-      } else {
-        res.status(200).json({
-          clientIp: fileInfo.clientIp,
-          localFilePath: fileInfo.localFilePath,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while fetching file information.' });
+  newData.save();
+  res.json("oke");
+});
+app.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(404).json("Wrong username!");
     }
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-  async function discover(clientIp) {
-    try {
-      await mongoose.connect('mongodb://localhost/MMT', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      const files = await File.find({ clientIp: clientIp });
-      const serverFileNames = files.map(file => file.serverFileName);
-      console.log('List of local files:', serverFileNames);
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
+    if (req.body.password != user.password) {
+      return res.status(404).json("Wrong password!");
     }
+    res.json(user);
+  } catch (error) {
+    res.json("dang nhap fail");
   }
-  
-const ping = require('ping');
+});
+
+// app.post("/publish", async (req, res) => {
+//   const { localFilePath, serverFileName } = req.body;
+//   const networkInterfaces = os.networkInterfaces();
+//   const clientIp = networkInterfaces["Wi-Fi"][1].address;
+//   try {
+//     const file = new File({ clientIp, localFilePath, serverFileName });
+//     await file.save();
+//   } catch (err) {
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while saving the file information." });
+//   }
+// });
+
+// app.post("/fet", async (req, res) => {
+//   try {
+//     const serverFileName = req.body;
+//     const fileInfo = await File.findOne({ serverFileName });
+//     if (!fileInfo) {
+//       res.status(404).json({ error: "File not found" });
+//     } else {
+//       console.log(fileInfo.clientIp);
+//       downloadFileFromIP(
+//         fileInfo.clientIp,
+//         3000,
+//         fileInfo.localFilePath,
+//         "C:\\test_btl\\download\\" + getLastNameFromPath(fileInfo.localFilePath)
+//       );
+//       res.json(fileInfo);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while fetching file information." });
+//   }
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+async function discover(clientIp) {
+  try {
+    await mongoose.connect("mongodb://localhost/MMT", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const files = await File.find({ clientIp: clientIp });
+    const serverFileNames = files.map((file) => file.serverFileName);
+    console.log("List of local files:", serverFileNames);
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+const ping = require("ping");
 function pingClient(clientIp) {
   return new Promise((resolve, reject) => {
     ping.sys.probe(clientIp, (isAlive) => {
       if (isAlive) {
-        console.log('Active');
+        console.log("Active");
       } else {
-        console.log('Inactive');
+        console.log("Inactive");
       }
     });
   });
 }
-  
-//Command shell
-const readline = require('readline');
+
+function downloadFileFromIP(ipAddress, port, filePath, localFilePath) {
+  const net = require("net");
+  const fs = require("fs");
+  const client = new net.Socket();
+  client.connect(port, ipAddress, () => {
+    client.write(`GET ${filePath}\r\n\r\n`);
+  });
+  client.on("data", (data) => {
+    fs.appendFileSync(localFilePath, data);
+  });
+}
+
+function getLastNameFromPath(path) {
+  const matches = path.match(/[^\\]+$/);
+  if (matches) {
+    return matches[0];
+  } else {
+    return null;
+  }
+}
+
+// Command shell
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: 'Command> '
+  prompt: "Command> ",
 });
 rl.prompt();
-rl.on('line', (line) => {
+rl.on("line", (line) => {
   const input = line.trim();
-  const args = input.split(' ');
+  const args = input.split(" ");
 
   if (args.length >= 1) {
     const command = args[0];
-    if (command === 'exit') {
+    if (command === "exit") {
       rl.close();
       return;
     }
-    if (command === 'discover') {
+    if (command === "discover") {
       if (args.length === 2) {
         const arg = args[1];
         discover(arg);
       } else {
-        console.log('Invalid command. Usage: discover arg');
+        console.log("Invalid command. Usage: discover arg");
       }
-    } else if (command === 'ping') {
+    } else if (command === "ping") {
       if (args.length === 2) {
         const arg = args[1];
         pingClient(arg);
       } else {
-        console.log('Invalid command. Usage: ping arg');
+        console.log("Invalid command. Usage: ping arg");
       }
     } else {
       console.log(`Command not recognized: ${command}`);
     }
   } else {
-    console.log('Invalid command.');
+    console.log("Invalid command.");
   }
   rl.prompt();
-}).on('close', () => {
-  console.log('Command shell đã đóng');
+}).on("close", () => {
+  console.log("Command shell đã đóng");
   process.exit(0);
 });
-  
